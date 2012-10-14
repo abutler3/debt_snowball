8d5cd6b0c12c4d83037a2df4db12548ae59eb798
Drew Butler <abutler3@gmail.com>
2012-10-14 09:08:50 -0500

    Making first test

--- lib/debt.rb
+++ lib/debt.rb
@@ -1,69 +1,16 @@
 require 'yaml'
 
-$debt_arr = Array.new
+class Debt
+  attr_accessor :name, :balance, :payment, :apr
 
-$fn = 'debt_db.yml'
-
-
-class DebtList
-
-    def initialize
-        @name          = arr[0]
-        @minimum       = arr[1]
-        @interest      = arr[2]
-        @total         = arr[3]
-    end
-
-    def getdetails
-        return[@name, @minimum, @interest, @total]
-    end
-end
-
-
-def newDebt
-  print( "Please enter the debt name: " )
-  d_name = gets().chomp()
-  print( "Please enter the minimum monthly payment: " )
-  d_pay = gets().chomp().to_i
-  print( "Please enter the interest. If none, enter 0: " )
-  d_interest = gets().chomp().to_i
-  print( "Please enter the total balance: " )
-  d_balance = gets().chomp().to_i
-  return [d_name, d_pay, d_interest, d_balance ]
-end
-
-
-def addDebt( add )
-    $debt_arr << add
-end
-
-def saveDB
-    File.open( $fn, 'w' ) {
-        |f|
-        f.write($debt_arr.to_yaml)
-    }
-end
-
-def loadDB
-    input_data = File.read( $fn )
-    $debt_arr = YAML::load( input_data )
-end
-
-def showData
-    puts( $debt_arr.to_yaml )
-end
-
-
-ans = ''
-until ans == 'q' do
-  puts( "Add (D)ebt, (P)ayment CD, (L)ist All - (S)ave or (Q)uit?" )
-  print( "> " )
-  ans = gets[0].downcase()
-  case ans
-  when 'd' then addDebt( DebtList.new( newDebt() ) )
-  when 'p' then puts "Payment made...."
-  when 'l' then puts "Listing..."
-  when 's' then saveDB
+  def initalize
+    @data = YAML.load_file("bills.yaml")
+    @extra_each_month = data["extra_each_month"]
+    @one_shot = data["one_shot_extra"]
+    @max_months = data["max_months"]
+    @all_bills = data["bills"]
+    puts @data
   end
-  showData
+
 end
+
--- test/test_snow.rb
+++ test/test_snow.rb
@@ -5,47 +5,47 @@ class SnowTest < Test::Unit::TestCase
 
 
     def test_01_saving_debt_name
-        ll = DebtList.new
-        assert_equal("Chase Bank", ll.newDebt[0])
-    end
-
-    def test_02_saving_debt_payment
-        ll = DebtList.new
-        assert_equal(200, ll.newDebt[1])
-    end
-
-    def test_03_saving_name_of_debt_balance
-        ll = DebtList.new
-        assert_equal(2000,  ll.newDebt[3])
-    end
-
-    def test_04_saving_interest
-        ll = DebtList.new
-        assert_equal(0,  ll.newDebt[2])
-    end
-
-    def test_05_column_header
         ll = Debt.new
-        assert_equal(nil, ll.create)
+        assert_equal("Chase Bank", ll.initialize)
     end
 
-    def test_06_subtract_payment_and_create_new_total
-        ll = Debt.new
-       assert_equal(1800, ll.record)
-    end
+   #  def test_02_saving_debt_payment
+   #      ll = DebtList.new
+   #      assert_equal(200, ll.newDebt[1])
+   #  end
 
-    def test_07_saving_debt_info
-        ll = Debt.new
-       assert_equal(1800, ll.record)
-    end
+   #  def test_03_saving_name_of_debt_balance
+   #      ll = DebtList.new
+   #      assert_equal(2000,  ll.newDebt[3])
+   #  end
 
-    def test_08_saving_second_debt_entry_to_array
-        ll = Debt.new
-       assert_equal(1800, ll.record)
-    end
+   #  def test_04_saving_interest
+   #      ll = DebtList.new
+   #      assert_equal(0,  ll.newDebt[2])
+   #  end
 
-    def test_09_total_debt_for_two_entries
-        ll = Debt.new
-       assert_equal(1800, ll.record)
-   end
+   #  def test_05_column_header
+   #      ll = Debt.new
+   #      assert_equal(nil, ll.create)
+   #  end
+
+   #  def test_06_subtract_payment_and_create_new_total
+   #      ll = Debt.new
+   #     assert_equal(1800, ll.record)
+   #  end
+
+   #  def test_07_saving_debt_info
+   #      ll = Debt.new
+   #     assert_equal(1800, ll.record)
+   #  end
+
+   #  def test_08_saving_second_debt_entry_to_array
+   #      ll = Debt.new
+   #     assert_equal(1800, ll.record)
+   #  end
+
+   #  def test_09_total_debt_for_two_entries
+   #      ll = Debt.new
+   #     assert_equal(1800, ll.record)
+   # end
 end
