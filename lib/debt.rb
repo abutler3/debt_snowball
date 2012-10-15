ade31bcb7f9456319c7f648cef103f194491c088
Drew Butler <abutler3@gmail.com>
2012-10-15 08:32:56 -0500

    Weekend work

--- lib/debt.rb
+++ lib/debt.rb
@@ -1,15 +1,45 @@
 require 'yaml'
 
+$data = YAML::load( File.open ('bills.yml') )
+
 class Debt
   attr_accessor :name, :balance, :payment, :apr
 
-  def initalize
-    @data = YAML.load_file("bills.yaml")
-    @extra_each_month = data["extra_each_month"]
-    @one_shot = data["one_shot_extra"]
-    @max_months = data["max_months"]
-    @all_bills = data["bills"]
-    puts @data
+  def initialize
+    @extra_each_month = $data["extra_each_month"]
+    @one_shot = $data["one_shot_extra"]
+    @max_months = $data["max_months"]
+    @all_bills = $data["bills"]
+  end
+
+  def extra
+    p @extra_each_month
+  end
+
+  def showData
+    puts $data.to_yaml
+  end
+
+  def one
+    p @one_shot
+  end
+
+  def showBills
+    puts @all_bills
+  end
+
+  def max
+    p @max_months
+  end
+
+  def r(val)
+    (val * 100).round / 100.0
+  end
+
+  def total_balance
+    r(@all_bills.inject(0) {|i, b|
+      i + b["balance"]
+      })
   end
 
 end
--- test/test_snow.rb
+++ test/test_snow.rb
@@ -4,9 +4,39 @@ require 'debt'
 class SnowTest < Test::Unit::TestCase
 
 
-    def test_01_saving_debt_name
+    def test_01_getting_extra
         ll = Debt.new
-        assert_equal("Chase Bank", ll.initialize)
+        assert_equal(40, ll.extra)
+    end
+
+    def test_02_print_info
+        ll = Debt.new
+        assert_equal(nil, ll.showData)
+    end
+
+    def test_03_print_one
+        ll = Debt.new
+        assert_equal(150, ll.one)
+    end
+
+    def test_04_print_bills
+        ll = Debt.new
+        assert_equal(nil, ll.showBills)
+    end
+
+    def test_05_max_number
+        ll = Debt.new
+        assert_equal(6, ll.max)
+    end
+
+    def test_06_compute_r
+        ll = Debt.new
+        assert_equal(9175, ll.r(9175))
+    end
+
+    def test_07_find_total_balance
+        ll = Debt.new
+        assert_equal(9175.0, ll.total_balance)
     end
 
 end
