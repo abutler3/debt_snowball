c8d26677db1ec47f43107d1db0a664c9e8e8ad93
Drew Butler <abutler3@gmail.com>
2012-10-16 09:10:25 -0500

    Added tests

--- lib/debt.rb
+++ lib/debt.rb
@@ -10,6 +10,7 @@ class Debt
     @one_shot = $data["one_shot_extra"]
     @max_months = $data["max_months"]
     @all_bills = $data["bills"]
+    @original_bill_count = @all_bills.length
   end
 
   def extra
@@ -40,6 +41,42 @@ class Debt
     r(@all_bills.inject(0) {|i, b|
       i + b["balance"]
       })
+  end
+
+  def find_max_name
+    max_name_length = @all_bills.inject(0) {|i, b| [i, b["name"].length].max }
+  end
+
+  def print_max_name
+    name_format = "%-#{find_max_name}s"
+  end
+
+  def save_o
+    original_balance = total_balance
+  end
+
+  def print_o
+    puts sprintf("ORIGINAL BALANCE: %.2f", save_o)
+    puts
+  end
+
+  def pay_off
+    m = 0
+      while (@all_bills.length > 0)
+        m += 1
+        extra_now = @extra_each_month + @one_shot
+        one_shot = 0
+        total_old_balance = total_balance
+        total_paid_this_month = 0
+        total_interest_this_month = 0
+        lowest_balance = nil
+        lowest_balance_name = nil
+        bills_paid_off_this_month = 0
+        puts sprintf("Month %d: (extra=%.2f)", m, extra_now)
+        @paid_off_bills.each do |paid_off_bill|
+          puts sprintf("#{name_format}  Paid %d months ago", paid_off_bill.name, m - paid_off_bill.month_paid_off)
+        end
+      end
   end
 
 end
--- test/test_snow.rb
+++ test/test_snow.rb
@@ -39,4 +39,33 @@ class SnowTest < Test::Unit::TestCase
         assert_equal(9175.0, ll.total_balance)
     end
 
+    def test_08_find_max_length
+        ll = Debt.new
+        assert_equal(12, ll.find_max_name)
+    end
+
+    def test_09_print_max_name_length
+        ll = Debt.new
+        assert_equal(12, ll.print_max_name)
+    end
+
+    def test_10_find_original_stuff_1
+        ll = Debt.new
+        assert_equal(9175.0, ll.save_o)
+    end
+
+    def test_11_find_original_stuff_2
+        ll = Debt.new
+        assert_equal(6, ll.save_o)
+    end
+
+    def test_12_print_starting_balance
+        ll = Debt.new
+        assert_equal(nil, ll.print_o)
+    end
+
+    def test_13_print_starting_balance
+        ll = Debt.new
+        assert_equal(nil, ll.pay_off)
+    end
 end
