09357fd3fd6cbedd99020b617ae1299dfc9bbc8b
Drew Butler <abutler3@gmail.com>
2012-10-18 11:45:54 -0500

    Changed from array to hash

--- lib/debt.rb
+++ lib/debt.rb
@@ -1,74 +1,54 @@
 require 'yaml'
 
-$debt_arr = Array.new
+$age_hash = { }  # start with an empty hash
 
-$fn = 'debt_db.yml'
-
-
-class DebtList
-
-    def initialize
-        @name          = arr[0]
-        @minimum       = arr[1]
-        @interest      = arr[2]
-        @total         = arr[3]
-    end
-
-    def getdetails
-        return[@name, @minimum, @interest, @total]
-    end
-end
-
-def newDebt
-  print( "Please enter the debt name: " )
-  d_name = gets().chomp()
-  print( "Please enter the minimum monthly payment: " )
-  d_pay = gets().chomp().to_i
-  print( "Please enter the interest. If none, enter 0: " )
-  d_interest = gets().chomp().to_i
-  print( "Please enter the total balance: " )
-  d_balance = gets().chomp().to_i
-  return [d_name, d_pay, d_interest, d_balance ]
-end
-
-
-def addDebt( add )
-    $debt_arr << add
-end
+  def input
+    print( "Please enter the debt name: " )
+    d_name = gets().chomp()
+    print( "Please enter the minimum monthly payment: " )
+    d_pay = gets().chomp().to_i
+    print( "Please enter the interest. If none, enter 0: " )
+    d_interest = gets().chomp().to_i
+    print( "Please enter the total balance: " )
+    d_balance = gets().chomp().to_i
+    $age_hash = {name: d_name, minimum: d_pay, interest: d_interest, total: d_balance}
+  end
 
 def saveDB
-    File.open( $fn, 'w' ) {
-        |f|
-        f.write($debt_arr.to_yaml)
-    }
+File.open('hash.yml', 'a'){ |f|
+  YAML.dump($age_hash, f)
+  }
 end
 
 def loadDB
-    input_data = File.read( $fn )
-    $debt_arr = YAML::load( input_data )
+  yp = File.open('hash.yml')
+log = YAML::load_documents( yp ) { |doc|
+  puts "#{doc[:name]}"
+  puts "#{doc[:minimum]}"
+  puts "#{doc[:total]}"
+}
 end
 
-def showData
-    puts( $debt_arr.to_yaml )
+def rand
+  a = ['a','b','z'].sample
 end
 
-def printData
-  data = YAML.load_file("debt_db.yml")
-  data.each do |bill|
-    print
-  end
-end
-
+#Start me up
 ans = ''
 until ans == 'q' do
-  puts( "Add (D)ebt, (P)ayment CD, (L)ist All - (S)ave or (Q)uit?" )
-  print( "> " )
+  puts "Good morning, would you like to get rid of some debt?"
+  puts
+  puts "Add (D)ebt, Record (P)ayment, (L)ist All - (S)ave or (Q)uit?"
+  puts
+  puts "Press (K) for the letter of the moment!!!"
+  print "> "
   ans = gets[0].downcase()
   case ans
-  when 'd' then addDebt( DebtList.new( newDebt() ) )
+  when 'd' then input
   when 'p' then puts "Payment made...."
-  when 'l' then puts "Listing..."
+  when 'l' then loadDB
   when 's' then saveDB
+  when 'k' then puts "The letter of the moment is #{rand}. Stop stalling!!!"
   end
-  showData
+  puts( "Thanks, now cut up those credit cards!!!")
 end
